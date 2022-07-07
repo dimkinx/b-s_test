3701d3bbdde39714f0cd57ba2e71fc0cf6cfdd90
Dmitry Ostroukhov <ha3i6opipgmy2j8pcscvidcyq8pvca@protonmail.com>
2022-07-07 14:53:31 +0300

    Solve task

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
-const main = (operations) => {
-    return null;
-}
+const main = (operations) => operations
+    .reduce(({ absProfit, portfolio }, { type, ticker, price, qnt }) => {
+        const currentProfit = (type === 'BUY' ? -1 : 1) * price * qnt;
+
+        return {
+            absProfit: absProfit + currentProfit,
+            portfolio: {
+                ...portfolio,
+                [ticker]: (portfolio[ticker] ?? 0) + currentProfit,
+            },
+        };
+    }, {
+        absProfit: 0,
+        portfolio: {},
+    });
 
 module.exports = main;
